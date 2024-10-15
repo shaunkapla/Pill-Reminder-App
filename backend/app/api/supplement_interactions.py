62c505b4c643cef65dfb421247e590ff7496d889
Shaun <kaplashaun@gmail.com>
2024-10-15 11:48:26 -0600

    inital structure for project and backend with some inital files and thoughts

---
+++ backend/app/api/supplement_interactions.py
@@ -0,0 +1,13 @@
+"""
+This file will deal with the exposed endpoints that the 
+front end will use for all things supplement related
+
+** Supplements can be pills, powders, injections, etc. **
+
+Endpoints:
+1. input_supp_info: This endpoint will be used to create information about a supplement
+2. delete_supp_info: This endpoint will be used to delete information about a supplement
+3. get_supp_info: This endpoint will be used to get information about one supplement
+4. get_all_supp_info: This endpoint will be used to get information about all supplements a user is taking
+5. update_supp_info: This endpoint will be used to update information about one supplement
+"""
---
+++ backend/app/api/user_interactions.py
@@ -0,0 +1,13 @@
+"""
+This file will deal with the exposed endpoints that the 
+front end will use for all things user related.
+
+Endpoints:
+1. create_user: This endpoint will create a user in the database
+2. get_user_info: This endpoint will get all the information related to the user
+3. get_user_login: This endpoint will get the users username and password for validation
+4. delete_user: This endpoint will remove a user from the database
+5. update_user_info: This endpoint will update the users information
+6. update_user_username: This endpoint will update the users username
+7. update_user_password: This endpoint will update the users password
+"""
