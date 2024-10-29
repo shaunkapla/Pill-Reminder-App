3f57ba95082bc5edb74efe34858a31f631998b25
Shaun <kaplashaun@gmail.com>
2024-10-29 15:30:57 -0600

    changed api to validate the user login credentials and give the relevant info

--- backend/app/dal/read/get_user_methods.py
+++ backend/app/dal/read/get_user_methods.py
@@ -9,8 +9,29 @@ def get_user_by_email(email):
         """
         cursor.execute(query, (email,))
         user = cursor.fetchone()
-        conn.close()
-        return user
+
+        return user if user else None
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+    finally:
+        if conn:
+            conn.close()
+
+def get_user_credentials_by_email(email):
+    try:
+        conn = db_connection()
+        cursor = conn.cursor()
+        query = """
+        SELECT email, pw FROM users WHERE email = ?
+        """
+        cursor.execute(query, (email,))
+        user_credentials = cursor.fetchone()
+
+        return user_credentials if user_credentials else None
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
     finally:
         if conn:
             conn.close()
